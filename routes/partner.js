e6192b91df6ff7ed51e62c98792b7b09a468219a
dorew9500-arch <dorew9500@gmail.com>
2026-06-28 13:21:58 +0900

    Add files via upload

--- routes/partner.js
+++ routes/partner.js
@@ -11,7 +11,7 @@ router.get('/', requirePartner, (req, res) => {
   });
   const rooms = query('SELECT r.*, s.name as store_name FROM rooms r JOIN stores s ON r.store_id = s.id WHERE r.is_active = 1 ORDER BY r.store_id, r.sort_order');
   const durations = query('SELECT * FROM duration_options ORDER BY sort_order');
-  const areas = query('SELECT DISTINCT t.id, t.name FROM area_tags t JOIN store_tags st ON t.id = st.tag_id ORDER BY t.sort_order');
+  const areas = query('SELECT id, name FROM area_tags ORDER BY sort_order');
   res.render('partner/dashboard', { user: req.session.user, stores, rooms, durations, areas });
 });
 
